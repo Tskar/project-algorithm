c7f980edae4307a5c3b12426341fd8fa0a16ab8f
Tskar <sanskarthapaofficial@gmail.com>
2021-12-22 15:43:03 -0500

    generate bars and bbsort algorithm

--- src/Components/AnimationTab/AnimationWindow.jsx
+++ src/Components/AnimationTab/AnimationWindow.jsx
@@ -1,11 +1,42 @@
 import './animationWindow.css'
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import BubbleSortAlgo from '../../Algorithms/BubbleSortAlgo';
 
 function AnimationWindow () {
 
+    const [array, setArray] = useState([]);
+
+    useEffect (() => {
+
+        createArray();
+    }, []);
+
+    const createArray=() => {
+        const array = [];
+
+        for (let i = 0; i < 10; i++) {
+            array.push(randomNumber(5, 200));
+        }
+
+        setArray(array);
+        console.log(array);
+    }
+
+    function randomNumber(minimumNumber, maximumNumber) {
+        return Math.floor(Math.random() * parseInt((maximumNumber-minimumNumber) + minimumNumber));
+    }
+    
+
     return (
         <div className='displayPage'>
-            Animation Window
+            {array.map((item, idx) => 
+                (<div key={idx} className='array-bars' 
+                    style={{height: `${item}px`}}>
+                        {item}
+                </div>)
+            )}
+            <button className='generate' onClick={() => createArray()}>Generate</button>
+            <BubbleSortAlgo array = {array} />
         </div>
     );
 };
